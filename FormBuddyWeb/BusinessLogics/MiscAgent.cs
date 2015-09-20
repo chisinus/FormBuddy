f145eba214720348a37805bc33f8e724cb2c006f
xiajun <chisinus@gmail.com>
2015-09-20 00:51:05 -0400

    2015/09/20

---
+++ FormBuddyWeb/BusinessLogics/MiscAgent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormBuddyWeb.BusinessLogics
+{
+    public class MiscAgent
+    {
+        public static List<SelectListItem> GetSecurityQuestions()
+        {
+            List<SelectListItem> ret = new List<SelectListItem>()
+            {
+                new SelectListItem { Text="What was your childhood nickname?", Value="1" },
+                new SelectListItem { Text="What school did you attend for sixth grade?", Value="2" },
+                new SelectListItem { Text="What was your dream job as a child?", Value="3" },
+                new SelectListItem { Text="What year did you graduate from High School?", Value="4" },
+                new SelectListItem { Text="What was the color of your first car?", Value="5" }
+            };
+
+            return ret;
+        }
+    }
+}
